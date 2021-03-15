06152bb9d21bb86a4f4f8a74b61d4776b342faa9
Paulo Henrique dos Reis Moura <paulo.moura@ambevtech.com.br>
2021-03-14 21:49:52 -0300

    Quering full item information from product service

--- cart/app.py
+++ cart/app.py
@@ -5,24 +5,43 @@ import json
 app = Flask(__name__)
 dapr_port = os.getenv('DAPR_HTTP_PORT')
 cart_store = 'cartstore'
+
 def product_exists(id):
+    """
+    verify if product exists in product service
+    """
     response = requests.get(f'http://localhost:{dapr_port}/v1.0/invoke/product/method/products/{id}')
     if response.status_code == 200: 
         return True
     return False
 
+def get_item(id):
+    """
+    query the item in product service
+    """
+    response = requests.get(f'http://localhost:{dapr_port}/v1.0/invoke/product/method/products/{id}')
+    if response.status_code == 200: 
+        return response.json()
+    return response.json()
+
 def add_to_cart_store(productId, userName):
+    """
+    Adds a product in the cart
+    """
     body = json.dumps([{
             'key': userName,
             'value':  productId
         }])
     response = requests.post(f'http://localhost:{dapr_port}/v1.0/state/{cart_store}',
-    json=body)
-    return response.content 
+    data=body)
+    return response.json()
 
 def get_from_cart_store(userName): 
+    """
+    Get the cart information on the store
+    """
     response = requests.get(f'http://localhost:{dapr_port}/v1.0/state/{cart_store}/{userName}') 
-    return response
+    return int(response.json())
 
 @app.route('/add',methods=['POST'])
 def add():
@@ -35,7 +54,9 @@ def add():
 @app.route('/cart',methods=['GET'])
 def get_cart():
     userName = request.args.get("user")
-    return get_from_cart_store(userName)
+    itemsId =  get_from_cart_store(userName)
+    return get_item(itemsId)
+    
 
 if __name__ == '__main__':
     app.run(port=5050)
